30ec1f70e39dd5a881b879935743c5bbb82295e2
Mark Karpeles <magicaltux@gmail.com>
2024-02-17 20:23:31 +0900

    implementaiton for Unique()

--- file.go
+++ file.go
@@ -17,7 +17,25 @@ func New() *File {
 	return &File{Header: h, Order: binary.NativeEndian}
 }
 
+type uniqueKey struct {
+	Key   string
+	Flags Flags
+}
+
 // Unique checks for any duplicate vlaues in Entries and ensures there is only one
-// entry for each filename + OS
+// entry for each filename + Flags
 func (f *File) Unique() {
+	found := make(map[uniqueKey]bool)
+	newEntries := make([]*Entry, 0, len(f.Entries))
+
+	for _, e := range f.Entries {
+		k := uniqueKey{e.Key, e.Flags}
+		if _, ok := found[k]; ok {
+			// already there, skip
+			continue
+		}
+		newEntries = append(newEntries, e)
+		found[k] = true
+	}
+	f.Entries = newEntries
 }
